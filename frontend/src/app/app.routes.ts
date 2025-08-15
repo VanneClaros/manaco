4085a3f43895e3343e57bc3503cae78e44a2770d
vane <vanessaclaros97@hotmail.com>
2025-08-14 22:33:32 -0400

    ANGULAR

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,18 @@
+import { Routes } from '@angular/router';
+import { Layout } from './layout/layout';
+import { Home } from './home/home';
+
+export const routes: Routes = [
+
+    {
+        path:'',
+        component:Layout,
+        children:[
+            {
+                path:'',
+                component:Home,
+                title:'Inicio'
+            }
+        ]
+    }
+];
